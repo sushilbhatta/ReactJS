fe775e65c0891b0dc53eabdeeca67596cc3ef271
sushilbhatta <sushilbhatta58@gmail.com>
2023-12-13 19:25:14 +0545

    react hook and useState intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("please click any topic");
+  function handleSelect(selectedButton) {
+    // selectedButton => 'component' 'jsx' 'props' 'state';
+    setSelectedTopic(selectedButton);
+    // console.log(selectedButton);
+    console.log(selectedTopic);
+  }
   return (
     <div>
       <Header />
@@ -24,14 +32,21 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
             {/* here the TabButton 
             component is the parent and the content inside the tab button is 'children' a special prop that React provide.
             here the children can simply be text, jsx code html code  */}
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            {/* using the arroe function is a common
+            way to pass the argument to the function in react as 
+            calling function by reference ie handleSelect()  directly
+            is not allowed .*/}
           </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
